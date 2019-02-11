dc4008c2bb336ca6f39ce237279b56921b2769fd
Virginia Dooley <vcdooley@gmail.com>
2019-02-11 15:33:30 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create]
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end 
+
 end
